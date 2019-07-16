dc91f85fbb21a469994d8848a4507c6d5322c5ee
ito-mutsumi <ito-mutsumi@dmm.com>
2019-07-16 13:11:11 +0900

    記述エラーを修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,10 @@ import css from '../assets/css/style.css';
 const IndexPages = () => {
   return (
     <h1 className={css.hoge}>
-      Welcome to 
-{' '}
-<b>Next.js</b>
-{' '}
-for TypeScript!
-</h1>
+      Welcome to
+      <b> Next.js </b>
+      for TypeScript!
+    </h1>
   );
 };
 
